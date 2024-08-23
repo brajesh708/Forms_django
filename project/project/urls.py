93a91df7b7766b9f62c3c8192f0da22be0f53a33
brajesh708 <brajeshmewada694@gmail.com>
2024-08-23 16:21:58 +0530

    urls

--- project/project/urls.py
+++ project/project/urls.py
@@ -19,7 +19,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('simpal',include('simpal.urls')),
-    path('basic',include('basic.urls')),
+    path('simpal/',include('simpal.urls')),
+    path('basic/',include('basic.urls')),
     
 ]
