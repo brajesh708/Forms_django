aaf1aaa6ad2a1ad158ccf08f76574783c3be2bd9
brajesh708 <brajeshmewada694@gmail.com>
2024-08-23 16:42:04 +0530

    views

--- project/basic/views.py
+++ project/basic/views.py
@@ -3,3 +3,22 @@ from django.shortcuts import render
 # Create your views here.
 def  home(request):
     return render(request, 'home.html')
+from django.shortcuts import render
+from .forms import RegistrationForm
+# Create your views here.
+
+def home(request):
+    form=RegistrationForm()
+    if request.method=="POST":
+        form=RegistrationForm(request.POST)
+        
+        if form.is_valid():
+            fname=form.cleaned_data["fname"]
+            lname=form.cleaned_data["lname"]
+            email=form.cleaned_data["email"]
+            contact=form.cleaned_data["contact"]
+            print(fname,lname,email,contact)
+            # data={"fname":fname,"lname":lname,"email":email,"contact":contact}
+            # Registration.objects.create(fname=fname,lname=lname,email=email,contact=contact)
+            form.save()
+    return render(request,"home.html",{"form":form})
