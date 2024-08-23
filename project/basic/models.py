28cc79bb0657c4edb0542afd654a641628218f17
brajesh708 <brajeshmewada694@gmail.com>
2024-08-23 16:35:21 +0530

    models

--- project/basic/models.py
+++ project/basic/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    name=models.CharField(max_length=50)
+    city=models.CharField(max_length=50)
+    email=models.EmailField()
+    contact=models.IntegerField()
