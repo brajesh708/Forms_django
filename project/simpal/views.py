bd39665846fd45a33f182899f9e3947090cfd384
brajesh708 <brajeshmewada694@gmail.com>
2024-08-25 15:37:41 +0530

    views

--- project/simpal/views.py
+++ project/simpal/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from .forms import *
 
 # Create your views here.
+
+
 def home(request):
-    return render(request, 'home.html')
+    form=Ragistration()
+    # return render(request,"home.html",{"form":form})
+    if request.method=="POST":
+        form=Ragistration(request.POST)
+        
+        if form.is_valid():
+            fname=form.cleaned_data["fname"]
+            lname=form.cleaned_data["lname"]
+            email=form.cleaned_data["email"]
+            contact=form.cleaned_data["contact"]
+            print(fname,lname,email,contact)
+            data={"fname":fname,"lname":lname,"email":email,"contact":contact}
+    return render(request,"home.html",{"form":form})
