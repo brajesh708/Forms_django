408ee45f487d15ca08a786016ec2c29ed138709e
brajesh708 <brajeshmewada694@gmail.com>
2024-08-25 13:12:19 +0530

    update

--- project/project/urls.py
+++ project/project/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/',admin.site.urls),
     path('simpal/',include('simpal.urls')),
     path('basic/',include('basic.urls')),
     
