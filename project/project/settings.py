a2aa9ef84d3dcc21242d69b4b901c79413df9db6
brajesh708 <brajeshmewada694@gmail.com>
2024-08-26 13:35:19 +0530

    SEETING

--- project/project/settings.py
+++ project/project/settings.py
@@ -42,6 +42,10 @@ INSTALLED_APPS = [
     'fromyou',
 ]
 
+AUTHNTICATION_BACKENDS=[
+    'django.contrib.auth.backends.ModelBackend',
+]
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
