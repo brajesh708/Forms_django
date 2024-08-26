a6d4b5f1481c8c8a7c2927a286648f9d794e0557
brajesh708 <brajeshmewada694@gmail.com>
2024-08-26 13:40:50 +0530

    views

--- project/fromyou/urls.py
+++ project/fromyou/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path
+from .views import *
 
 urlpatterns = [
     path('admin/',admin.site.urls),
+    path('',ragister_views,name='ragister'),
+    path('login',login_views,name='login')
     
 ]
--- project/fromyou/views.py
+++ project/fromyou/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def ragister_views(request):
+    return render(request, 'register.html')
+
+def login_views(request):
+    return render(request, 'login.html' )
+
