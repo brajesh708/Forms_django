a84b8a04f5a0f9a4b8810b19f7c306e8e1aac5a9
brajesh708 <brajeshmewada694@gmail.com>
2024-08-27 17:20:11 +0530

    url

--- project/cratefrom/urls.py
+++ project/cratefrom/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
+from .views import *
 
 urlpatterns = [
-    path('admin/',admin.site.urls),
-   
-    
+    path('admin/', admin.site.urls),
+    path('',home,name='home'),
+    path("login/",login,name='login')
 ]
