2d800ef00b9a3fa6cd271884f12ace776e25fed3
brajesh708 <brajeshmewada694@gmail.com>
2024-08-30 17:35:20 +0530

    update

--- project/cratefrom/urls.py
+++ project/cratefrom/urls.py
@@ -13,5 +13,5 @@ from cratefrom import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name='home'),
-    path("login/",views.login,name='login')
+    path("login/",views.login,name='login'),
 ]
